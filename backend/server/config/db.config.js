cd9acfda37cf8dcaef43b60b27e5259ffa3c1e1a
Tokued <anthonybernabel3@gmail.com>
2026-03-04 12:41:35 -0500

    quick fix to test mine and everyone elses

--- backend/server/config/db.config.js
+++ backend/server/config/db.config.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
+mongoose.set("strictQuery", false); // removes the deprecation warning
+
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("The backend has connected to the MongoDB database.");
   } catch (error) {
     console.log(`${error} could not connect`);
-    throw error; // Important for tests
+    throw error;
   }
 };
